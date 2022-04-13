724387da3f0a6d995f6108cde88faa3cef3731df
Sambo Amaza <38713132+samaboamaza@users.noreply.github.com>
2022-04-13 09:37:54 -0500

    Create shapeArea.py

---
+++ Python/CodeSignal/Intro/Edge of the Ocean/shapeArea.py
@@ -0,0 +1,9 @@
+'''
+Below we will define an n-interesting polygon. Your task is to find the area of a polygon for a given n.
+A 1-interesting polygon is just a square with a side of length 1. An n-interesting polygon is obtained by taking the n - 1-interesting polygon and 
+appending 1-interesting polygons to its rim, side by side.
+'''
+
+def solution(n):
+    return n**2 + (n-1)**2
+    
