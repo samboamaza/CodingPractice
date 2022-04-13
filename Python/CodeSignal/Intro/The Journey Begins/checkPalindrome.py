3fe5706c199b24a804433dfc0f170529dca0cf94
Sambo Amaza <38713132+samaboamaza@users.noreply.github.com>
2022-04-13 09:35:40 -0500

    Create checkPalindrome.py

---
+++ Python/CodeSignal/Intro/The Journey Begins/checkPalindrome.py
@@ -0,0 +1,17 @@
+#Given the string, check if it is a palindrome.
+
+'''
+Palindrome
+A string that doesn't changed when reversed (it reads the same backward and forward).
+
+Examples:
+
+"eye" is a palindrome
+"noon" is a palindrome
+"decaf faced" is a palindrome
+"taco cat" is not a palindrome (backwards it spells "tac ocat")
+"racecars" is not a palindrome (backwards it spells "sracecar")
+'''
+def solution(inputString):
+    return inputString == inputString[::-1]
+
