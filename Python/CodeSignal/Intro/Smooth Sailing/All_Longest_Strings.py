7367d93a0ff132e868b8ec9d22f1a6e7d65fa6dc
Sambo Amaza <38713132+samboamaza@users.noreply.github.com>
2022-04-21 14:53:02 -0500

    Update All_Longest_Strings.py

--- Python/CodeSignal/Intro/Smooth Sailing/All_Longest_Strings.py
+++ Python/CodeSignal/Intro/Smooth Sailing/All_Longest_Strings.py
@@ -1,1 +1,12 @@
 # Given an array of strings, return another array containing all of its longest strings.
+
+def solution(inputArray):
+    result= []
+    max_len = max(len(x) for x in inputArray)
+    for i in inputArray: 
+        if len(i) == max_len: 
+            result.append(i)
+    return result
+    
+        
+
