b1c597c6bebce648d8803fb77a1cef75bf6ac0a4
Sambo Amaza <38713132+samboamaza@users.noreply.github.com>
2022-07-03 21:19:33 -0500

    Update itemCounts.sql

--- SQL/CodeSignal/GROUP Dishes BY Type/itemCounts.sql
+++ SQL/CodeSignal/GROUP Dishes BY Type/itemCounts.sql
@@ -1,3 +1,21 @@
+/*
+You recently started working in the IT department of a large store. You were put in charge of the inventory database availableItems, which has the 
+following structure:
+
+id: unique item ID;
+item_name: the name of the item;
+item_type: the type of the item.
+Note that it is possible for items that are of different types to have the same names.
+
+One of the most common operations performed on this database is querying the number of specific items available at the store. Since the database is quite 
+large, queries of this type can take up too much time. You have decided to start analyzing this problem by counting the number of each available item.
+
+Given the availableItems table, write a select statement which returns the following three columns: item_name, item_type and item_count, containing the 
+names of the items, their types, and the amount of those items, respectively. The output should be sorted in ascending order by item type, with items of 
+the same type sorted in ascending order by their names.
+
+*/
+
 CREATE PROCEDURE solution()
 BEGIN
 	/* Write your SQL here. Terminate each statement with a semicolon. */
